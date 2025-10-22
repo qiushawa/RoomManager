113a12d2f25d88e7dea77fc7d4bdc6e1d91e92e9
kuanyu <kuanyu20070225@gmail.com>
2025-10-22 08:42:30 +0800

    新增kuanyu頁面

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+
+// kuanyu
+Route::get('/kuanyu', function () {
+    
+    return Inertia::render('Kuanyu');
+})->name('kuanyu');
