a1ba6c4ac226c0d608275327b37fc6b7a720be68
Qiushawa <qiushawa@gmail.com>
2025-10-24 02:51:45 +0800

    新增 Blacklist 模型及其資料表遷移，包含事由編號和內容欄位

---
+++ app/Models/Blacklist.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Classroom Model
+ *
+ * 此模型用於操作 classroom 資料表，對應教室資訊。
+ */
+class Blacklist extends Model
+{
+    public $keyType = 'int';
+    public $incrementing = false;
+    public $timestamps = false;
+    protected $table = 'blacklist_reason';
+    protected $primaryKey = 'reason_id';
+    protected $fillable = [
+        'reason_id',
+        'reason',
+    ];
+}
