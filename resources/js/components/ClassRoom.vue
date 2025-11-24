fe773626e9bc9c5334fe8f8b38d12b4324388f48
kuanyu <kuanyu20070225@gmail.com>
2025-11-24 11:41:59 +0800

    classroom列表

---
+++ resources/js/components/ClassRoom.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <div v-for="classroom in classroom" :key="classroom.name" class="flex flex-col gap-4">
+      <h1>{{ classroom.name }}</h1>
+      <ul>
+        <li
+          v-for="item in classroom.items"
+          :key="item.id">
+          <button
+            @click="$emit('item-click', item)"
+            class="bg-gray-500 p-2 h-10 w-50 text-white flex items-center justify-center"
+            type="button"
+          >
+            {{ item.title }}
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  classroom: {
+    type: Array,
+    required: true
+  }
+})
+</script>
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,44 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+
+Route::get('kuanyuu', function () {
+    return Inertia::render('Kuanyuu');
+});
+
+
+
+// classrooms
+Route::get('/classroom', function () {
+    $classroom = [
+        [
+            'name' => '綜三館BGC',
+            'items' => [
+                ['id' => 1, 'title' => 'BGC0305'],
+                ['id' => 2, 'title' => 'BGC0402'],
+                ['id' => 3, 'title' => 'BGC0501'],
+                ['id' => 4, 'title' => 'BGC0508'],
+                ['id' => 5, 'title' => 'BGC0513'],
+                ['id' => 6, 'title' => 'BGC0601'],
+            ],
+        ],
+        [
+            'name' => '跨領域BCB',
+            'items' => [
+                ['id' => 7, 'title' => 'BCB0303'],
+                ['id' => 8, 'title' => 'BCB0305'],
+            ],
+        ],
+        [
+            'name' => '科研大樓BRA',
+            'items' => [
+                ['id' => 9, 'title' => 'BRA0102'],
+                ['id' => 10, 'title' => 'BRA0201'],
+            ],
+        ],
+    ];
+
+    // 使用 Inertia 回傳 Vue 頁面
+    return Inertia::render('Classroom')->with('classrooms', $classroom);
+})->name('classroom');
