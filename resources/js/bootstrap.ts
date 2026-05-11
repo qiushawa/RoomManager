5ea7897b1754b36dfcc0c58fec72aad469ef1b2b
Qiushawa <qiushawa@gmail.com>
2026-05-11 21:32:40 +0800

    fix: 更新 API 端點為函數形式以支援動態路徑，並優化 getAppUrl 和 getAppBasePath 的快取邏輯

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,10 +1,4 @@
 import axios from 'axios';
-import { getAppUrl } from './utils';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-const appUrl = getAppUrl();
-if (appUrl) {
-    window.axios.defaults.baseURL = appUrl;
-}
